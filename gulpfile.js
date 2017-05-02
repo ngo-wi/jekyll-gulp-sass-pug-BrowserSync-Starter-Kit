639e4d472dac1c8ce519f31f4d208bd529e30923
ngo-wi <petergabrielemail@gmail.com>
2017-05-02 08:10:27 +0300

    checking my spawn

--- gulpfile.js
+++ gulpfile.js
@@ -16,12 +16,11 @@ var messages = {
  * Build the Jekyll Site
  */
 
- gulp.task('jekyll-build', function(done){
- 	browserSync.notify(messege.jekyllBuild);
- 	return cp.spawn( jekyll , ['build'], {stdio: 'inherit'});
- 	    .on('close', done);
- });
-
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
 
 
 
